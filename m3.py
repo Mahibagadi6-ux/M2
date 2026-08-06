d07ae03ac0cf72100eb1506a58b5a0c2919c7936
Mahesh_bagadi <rockbodymaheshbagadi@gmail.com>
2026-08-06 10:11:51 +0530

    intialization

---
+++ m3.py
@@ -0,0 +1,19 @@
+# DATA TYPES RULES ON THIS VARIABLE NAME
+name = "mahesh"
+age = 22
+is_student = True
+weight = 80
+height = 60
+print(name)
+print(age)
+print(is_student)
+print(weight)
+print(height)
+
+#TYPE CONVERSION
+age = 22
+height = "19"
+print(age + int(height))
+
+
+
