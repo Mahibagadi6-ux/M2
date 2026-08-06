6510386865f8a10e106aa6b3820aeb23bc22904b
Mahesh_bagadi <rockbodymaheshbagadi@gmail.com>
2026-08-06 10:14:57 +0530

    intialization

---
+++ m4.py
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b) # it gives before decimal points
+print(a % b) # reminder only prints
+print(a ** b) # form of the power
