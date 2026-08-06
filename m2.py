f0dfd784f5162f58ce5c7c96036d59c0394d6226
Mahesh_bagadi <rockbodymaheshbagadi@gmail.com>
2026-08-06 10:01:32 +0530

    intialization

---
+++ m2.py
@@ -0,0 +1,19 @@
+# VARIABLES
+a = 20
+b = 30
+print(a)
+print(b)
+print(a + b)
+
+# we can re assign the value from diffrent number, if a = 20 in next line a = 30 run the code output must be 30
+a = 30
+a = 40
+print(a)
+
+# we can assign variables in one line
+a, b, c, d = 10, 20, 30, 40
+print(a)
+
+a = b = c = d = 10
+print(a)
+
