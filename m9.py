02cc51d3c204d7b74c378fb4239e9c557dc05c1f
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-08-07 13:12:07 +0530

    intialization

---
+++ m9.py
@@ -0,0 +1,16 @@
+# HOME WORK
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(" hello " + name + "your age is " + str(age))
+print(f"{name} your age is {age}")
+
+name = input("Enter your name: ")
+print(name.replace(" ", "_"))
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.strip())
+
+name = input("Enter your name: ")
+print(f"The number of the letters is {len(name)}")
+
