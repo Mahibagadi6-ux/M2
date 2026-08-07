559d502b784f8e70e1b5a3a86d522d26ce413bdb
Mahesh_bagadi <rockbodymaheshbagadi@gmail.com>
2026-08-07 11:41:43 +0530

    intialization

---
+++ m7.py
@@ -0,0 +1,9 @@
+# string method
+full_name = "mahesh bagadi"
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.strip())
+print(full_name.replace("mahesh bagadi","vishwa bagadi"))
+print(full_name.split())
+print("_".join(full_name))
+print(len(full_name))
