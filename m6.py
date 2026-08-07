589fa84ab28fb7852c976be4304e037a6653833d
Mahesh_bagadi <rockbodymaheshbagadi@gmail.com>
2026-08-07 11:33:09 +0530

    intialization

---
+++ m6.py
@@ -0,0 +1,12 @@
+# STRING MANUPULATION
+# 1)concatanation
+first_name = "mahesh"
+last_name = "bagadi"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+# 2)repetation
+message = " alert " * 3
+print(message)
+print(message * 3)
