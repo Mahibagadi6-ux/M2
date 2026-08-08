038f70e946207915edaa4805a9418dbedf46a546
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-08-09 00:27:20 +0530

    intialization

--- m10.py
+++ m10.py
@@ -31,5 +31,19 @@ print(not(a > 10))
 
 
 # membership operater
+name = "mahesh"
+my_list = ["mahi","bagadi","patil"]
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print("s" in name)
+print("mahi" in my_list)
+print(3 not in numbers)
+print("dev" not in my_list)
 
+
+# betwise operater
+a = 2
+b = 3
+print(a & b)
+print(a | b)
+print(a ^ b)
 
