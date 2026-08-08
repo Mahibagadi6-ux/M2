e2ccbedcba8a5fcbdff69946b287295efb50f691
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-08-08 23:52:24 +0530

    intialization

---
+++ m10.py
@@ -0,0 +1,35 @@
+# ASSIGNMENT OPERATER
+a = 10
+b = 20
+a += b
+a -= b
+a *= b
+a /= b
+print(a)
+
+# COMPARISION OPERATER
+a = 10
+b = 20
+print(a==b)
+print(a!=b)
+print(a > b)
+print(a >= b)
+print(a <= b)
+print(a <= 15)
+print(a == 15)
+print(a != 15)
+print(a != a)
+
+
+# logical operater
+a = 10
+b = 30
+print(a < 10 and  b > 30)
+print(a > 10 or b > 30)
+print(not(a<10))
+print(not(a > 10))
+
+
+# membership operater
+
+
