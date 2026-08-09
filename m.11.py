09081fe7f986bdcac6a5b7b3c053d32ecffe80f8
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-08-09 11:38:13 +0530

    intialization

---
+++ m.11.py
@@ -0,0 +1,34 @@
+# HW WORKS ON THE
+num_1 = int(input("Enter one  the number:"))
+num_2 = int(input("Enter two  the number:"))
+print(num_1 > 10 and num_2 >10)
+print(num_1 < 5 or num_2 < 5)
+print(not(num_1 >num_2))
+
+
+NUM_1 = int(input("Enter one  the number:"))
+NUM_2 = int(input("Enter two  the number:"))
+print(NUM_1 % 2 == 0 and NUM_2 % 2 == 0)
+print(NUM_1 > 20 or NUM_2 > 20)
+print(not(NUM_1 > NUM_2))
+
+name1 = int(input("Enter your name:"))
+name2 = int(input("Enter your age:"))
+print(name1<50 and name2<50)
+print(name1>=10 or name2>=10)
+print(not(name1>100))
+
+age = int(input("Enter your age:"))
+marks = int(input("Enter your marks:"))
+print("both numbers are even ", age>18 and marks>50)
+print("age is below 18 or marks are below 50", age<18 and marks<35)
+print("age is not grater than 60",not(age>60))
+
+
+num1 = int(input("Enter one  the number:"))
+num2 = int(input("Enter two  the number:"))
+num3 = int(input("Enter three  the number:"))
+print(num1>=10 and num2>10 and num3>10)
+print(num1<5 or num2<5 or num3<5 )
+print(not(num1>num3))
+
--- m7...py
+++
@@ -1,9 +0,0 @@
-# string method
-full_name = "mahesh bagadi"
-print(full_name.upper())
-print(full_name.lower())
-print(full_name.strip())
-print(full_name.replace("mahesh bagadi","vishwa bagadi"))
-print(full_name.split())
-print("_".join(full_name))
-print(len(full_name))
