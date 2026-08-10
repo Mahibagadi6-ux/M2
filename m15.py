590525722d794755012ee688ff370c4f8dfdf0f2
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-08-10 23:04:54 +0530

    intialization

---
+++ m15.py
@@ -0,0 +1,17 @@
+# SLICING TOPIC IS THE MOST IMP TOPIC SO LERN CLEARLY
+num = [10,5,6,3,8,90]
+num.sort()
+print(num) # i makes the assending order
+num.reverse() # it makes disending order
+print(num)
+
+
+""" matrics : already you learnt about matrix as simple as 
+that also working pyhton but one thing is that is index  
+ list in list but one thing dont forget about the cama(,) althoug shows error """
+mathrix = [
+          [11,2,3],
+          [22,5,77],
+          [233,45,56]
+          ]
+print(mathrix[0])
