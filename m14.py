6402fdf7077dcdc4b035a58a44122a8f717eac84
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-08-10 22:47:18 +0530

    intialization

---
+++ m14.py
@@ -0,0 +1,17 @@
+""""" WE ARE DISCUSSING ABOUT THE LIST ,, LIST means is collection of itmes ,,
+ IN LIST LOT OF METHODS ARE THRE CHECK WHAT ARE THEY  """
+dialy_th = ["collge dress" , "perfume" ,"best" ,"bag" ]
+print(dialy_th)
+print(dialy_th[0])
+print(dialy_th[1])
+print(dialy_th[2])
+dialy_th.append("shoos")
+print(dialy_th)
+dialy_th.pop()
+print(dialy_th)
+dialy_th.pop(0)  # means which index we will give , that would be disapper
+print(dialy_th)
+dialy_th.remove("perfume")  # it uses while only we wnat remove the specific element
+print(dialy_th)
+dialy_th.clear()
+print(dialy_th)
