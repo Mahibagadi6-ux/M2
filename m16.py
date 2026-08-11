8fe2cd21187d3f99e250aafd26855a5993a2c15d
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-08-11 13:30:00 +0530

    intialization

---
+++ m16.py
@@ -0,0 +1,38 @@
+# WE ARE DISCUSS ABOUT THE TUPLES FIRST
+# TULES ARE THE IMMUTABLE MEANS WE CANNOT CHANGE ELEMENTS AFTER INTIALIZATION AN ORDERD AND COLLECTION OF ITHEMS
+# TUPLES CAN BE REPRESNTEDED AS ()
+a = (1,2,3,4,5,6,7,8,9)
+print(a)
+print(a[1])
+print(a[0:9])
+print(a[8])
+print(insert(1,9)) # this line error due to tules not allow or immutable
+print(append.a(10)) # it also not allow to the becuase cannot allow any exter elemts
+
+
+ # we can also adding number by using others
+l = (1,2,3,4,5,6,7,8,9)
+list = list(l)
+print(list)
+
+ # tuples using we operates the artimetic operation like
+l1 = (1,2,3,4,5,6,7,8,9)
+l2 = (11,12,13,14,15,16,17,18,19,20)
+l1 += l2
+print(l1)
+
+# tuple susing repitation also allows
+l4 = (1,2,3,4,5,6,7,8,9) * 10
+print(l4)
+
+
+#tuples allows checking memebres
+l5 = (1,2,3,4,5,6,7,8,9)
+print(1 in l5)
+print(5 not in l5)
+print(not(15))
+
+print(l5.count(15))
+
+# one thing we want remebres we can put tuples in tuples
+
