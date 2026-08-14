2f5b1435854f65e1337194fa8647fdd91687957c
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-08-14 19:57:25 +0530

    intialization

---
+++ practice.py
@@ -0,0 +1,23 @@
+num = 0
+while num <= 10:
+    num = num + 1
+    print(num)
+
+num = 1
+while num <= 10:
+    i = 1
+    while i<=1:
+        if num%2==0:
+            print(num)
+        i = i + 1
+    num = num + 1
+
+num = 1
+while num <= 10:
+    i = 1
+    while i<=1:
+        if num%2 != 0:
+            print(num)
+        i = i + 1
+    num = num + 1
+
