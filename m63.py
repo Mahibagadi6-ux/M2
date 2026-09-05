b40e13c1f88a0580292833e0e5bd494a82ea96e4
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-09-06 00:19:34 +0530

    intialization

--- m63.py
+++ m63.py
@@ -2,8 +2,9 @@ class ATMmachineSBI:
     def __init__(self):
         self.pin = ""
         self.balance = 0
+        self.menu()
     def menu(self):
-        user_input = input("tell me how can i help you\n 1,press to creat the pin\n 2,change pin \n 3, withdrow the amount\n 3,Exite ")
+        user_input = input("tell me how can i help you\n 1,press to creat the pin\n 2,change pin \n 3, withdrow the amount\n 4,Exite \n 5,enetr above one option \n >>>>")
         if user_input == "1":
             self.creat_pin()
             pass
@@ -19,26 +20,28 @@ class ATMmachineSBI:
         else:
             print("you choose the invalid option")
     def creat_pin(self):
-        creat_pin = input("enter the your pin: >>")
+        creat_pin = int(input("enter the your pin: >>"))
         self.pin =  creat_pin
-        user_balance = int("Enter your balance: >>>")
+        user_balance = input("Enter your balance: >>>")
         self.balance = user_balance
-        print("your pin is created successfully")
+        print(f"your pin is created successfully and your balance is {self.balance}")
         self.menu()
     def change_pin(self):
         old_pin = input("enter the new pin to change: >>")
         if old_pin == self.pin:
             new_pin = input("enter the new pin to change: >>")
             self.pin = new_pin
-            print(f"your pin is changed successfully and your pin is {new_pin}")
+            print(f"your pin is changed successfully{old_pin} and your pin is {new_pin} ")
         else:
-            print(f"your entered old pin is {old_pin} and new pin is")
-            self.menu()
+            print(f"your rentered  you old pin is {old_pin} please enter your new pin ")
+        self.menu()
     def check_balance(self):
         user_pin = input("Enter your pin to check the balance: >>")
         if user_pin == self.pin:
             print(f"your balance is {self.balance}")
-            self.menu()
+        else:
+            print("invallid pin you entered")
+        self.menu()
     def withdrow_balance(self):
         user_pin = input("Enetr your pin to withdrow the balance: >>")
         if user_pin == self.pin:
@@ -48,8 +51,10 @@ class ATMmachineSBI:
                 print(f"your withdrow amount{self.amount}  and your balance is {self.balance}")
             else:
                 print("your accont in in sufficient amount ")
-                self.menu()
+        self.menu()
 sbi = ATMmachineSBI()
+
+
 
 
 
