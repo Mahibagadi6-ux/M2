676b479595b6eb9df3ca09c37c32e1a4cf22cee3
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-09-14 00:44:35 +0530

    intialization

--- m93.py
+++ m93.py
@@ -1,7 +1,20 @@
-from datetime import date
-from currency_converter import CurrencyConverter
-c = CurrencyConverter()
-print(c.convert(100,"EUR","INR",date=date(2025, 9, 1)))
-atm = float(input("\nEnter the amount in USD :>>"))
-new_atm = c.convert(atm,"USD","INR",date=date(2025, 8, 13))
-print(new_atm)
+# from datetime import date
+# from currency_converter import CurrencyConverter
+# c = CurrencyConverter()
+# print(c.convert(1,"USD","INR" ))
+# atm = float(input("\nEnter the amount in USD :>>"))
+# new_atm = c.convert(atm,"EUR","INR",date=date(2026, 9, 3))
+# print(new_atm)
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data("https://www.python.org/")
+qr.make(fit=True)
+img = qr.make_image(fill_color="yellow", back_color="black")
+img.save("m93.png")
+
+
---
+++ m94.py
@@ -0,0 +1,27 @@
+#match case statement
+num  = int(input("Enter a number: >>"))
+match num:
+    case 1:
+        print("one")
+    case 2:
+        print("two")
+    case 3:
+        print("three")
+    case 4:
+        print("four")
+    case 5:
+        print("five")
+    case _:
+        print("print some other number")
+day = input("Enter the day: ".lower())
+match day:
+    case "monday":
+        print("work day")
+    case "tuesday" | "wednesday":
+        print("week middle days")
+    case "thursday" | "friday" | "saturday":
+        print("almost weekend")
+    case "saturday" | "sunday":
+        print("weekend")
+    case _:
+        print("other week day")
