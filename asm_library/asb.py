03b7199188b76aaf2e54d960505a4baa93d6e86b
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-09-13 13:06:19 +0530

    Added my own Python library

---
+++ asm_library/asb.py
@@ -0,0 +1,6 @@
+from asm_library.addtion import addtion
+from asm_library.substarcton import sub
+from asm_library.multiplaction import mult
+print(addtion(10,20))
+print(sub(10,20))
+print(mult(10,20))
