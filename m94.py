6a786dc1e6d1d8012d906caf733c8f574d7da42c
Mahibagadi6-ux <mahibagadi6@gmail.com>
2026-09-14 00:51:11 +0530

    intialization

--- m94.py
+++ m94.py
@@ -25,3 +25,18 @@ match day:
         print("weekend")
     case _:
         print("other week day")
+
+time = int(input("Enter the time: "))
+match time:
+    case 8:
+        print("break fast time ")
+    case 9:
+        print(" want to go to the collage ")
+    case 10:
+        print("class listion time ")
+    case 11:
+        print("small break time  ")
+    case 12 | 13:
+        print(" rest time")
+    case _:
+        print("study time ")
